489a01aa1a4cb64ff1aee09335e6a64cea7b1eac
unknown <tomalhossen78@gmail.com>
2025-12-08 11:42:36 +0600

    delete button functionality done

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import { Outlet } from "react-router";
 import Footer from "../Components/Footer/Footer";
+import ScrollToTop from "../Utility/ScrollTop";
 
 const MainLayout = () => {
   return (
     <div>
+      <ScrollToTop />
       <Navbar />
       <main className="min-h-screen">
         <Outlet />
