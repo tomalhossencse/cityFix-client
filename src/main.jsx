2778effd034f1b8f0abe5d2477fa0e98ced2d746
unknown <tomalhossen78@gmail.com>
2025-12-09 23:51:09 +0600

    some issues solved

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <RouterProvider router={router}></RouterProvider>
-      </AuthProvider>
-    </QueryClientProvider>
-  </StrictMode>
+  // <StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
+  </QueryClientProvider>
+  // </StrictMode>
 );
