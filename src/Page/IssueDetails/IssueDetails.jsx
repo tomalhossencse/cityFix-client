9f3363d84d912402359788e29f9373cba1e16e17
unknown <tomalhossen78@gmail.com>
2025-12-14 11:43:27 +0600

    timeline and tracking aded

--- src/Page/IssueDetails/IssueDetails.jsx
+++ src/Page/IssueDetails/IssueDetails.jsx
@@ -99,8 +99,8 @@ const IssueDetails = () => {
 
   const statusColor = {
     pending: "bg-yellow-600 text-white",
-    rejected: "text-red-500",
-    working: "text-pink-600",
+    rejected: "bg-red-500 text-white",
+    working: "bg-pink-600 text-white",
     "in-progress": "bg-blue-600 text-white",
     resolved: "bg-green-600  text-white",
     closed: "bg-gray-500  text-white",
@@ -331,6 +331,72 @@ const IssueDetails = () => {
           </div>
         </div>
       </div>
+
+      {/* timeline and tracking */}
+      <div className="my-20">
+        <div className="text-center mb-12">
+          <span className="text-base-100 bg-primary rounded-lg px-6 py-3 font-bold text-xl">
+            Timeline & Tracking
+          </span>
+        </div>
+        <ul className="flex flex-col-reverse timeline timeline-snap-icon  max-md:timeline-compact timeline-vertical">
+          {issue?.timeline.map((item, index) => {
+            const isLeft = index % 2 === 0;
+
+            return (
+              <li key={index}>
+                <hr />
+                <div className="timeline-middle">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className={`h-8 w-8 ${
+                      statusColor[item?.status]
+                    } p-1 rounded-full`}
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+
+                <div
+                  className={`${
+                    isLeft
+                      ? "timeline-start mb-10 md:text-end"
+                      : "timeline-end md:mb-10"
+                  }`}
+                >
+                  <time className="font-bold italic">
+                    {DateFormat(item?.createdAt)}
+                  </time>
+                  <div
+                    className={`${
+                      isLeft ? "justify-end" : "justify-start"
+                    } text-lg font-black flex gap-1 items-center ${
+                      statusColor[item?.status]
+                    } p-2 rounded-md`}
+                  >
+                    <span> {statusIcon[item?.status]}</span>
+                    <span> {CapitalizeFirstLetter(item?.status)}</span>
+                  </div>
+                  <div>
+                    <p>{item?.message}</p>
+                    <div className="font-bold">
+                      Update By : {item?.updatedBy?.role}
+                    </div>
+                  </div>
+                </div>
+                <hr />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
       <IssueEdit issue={issue} modelRef={modelRef} refetch={refetch} />
     </Container>
   );
