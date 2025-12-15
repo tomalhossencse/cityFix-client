df7c907a7f14d43edcb71ba7bfe825c793626a01
unknown <tomalhossen78@gmail.com>
2025-12-15 19:45:26 +0600

    category section done

--- src/Page/Home/CategorySection/CategorySection.jsx
+++ src/Page/Home/CategorySection/CategorySection.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import useAxiosSecure from "../../../Hook/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
-
 const CategorySection = () => {
   const axiosSecure = useAxiosSecure();
   const { data: categories = [] } = useQuery({
